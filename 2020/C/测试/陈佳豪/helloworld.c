ec9d2fcc433f0b931094ecf1f403f53a530ff630
xinil <1223176327@qq.com>
2021-04-19 23:45:30 +0800

    链表，栈，队列考核任务 (#18)

--- 2020/C/测试/陈佳豪/helloworld.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-    printf("HELLO 3G_server!");
-    printf("HELLO 3G!");
-    return 0;
-}
--- 2020/c/helloworld/helloworld.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main(void) {
-	printf("helloworld");//Êä³öhelloworld
-	return 0;
-}
