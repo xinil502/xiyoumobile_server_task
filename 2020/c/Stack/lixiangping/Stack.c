2dfbcb44ec5b2ee3d5509ee0f002aab4413a3e9c
calll1 <1332875394@qq.com>
2021-04-26 18:45:33 +0800

    Update Stack.c
    栈

--- 2020/c/Stack/lixiangping/Stack.c
+++ 2020/c/Stack/lixiangping/Stack.c
@@ -1,8 +1,8 @@
 #include<stdlib.h>
 #define max 50
-
+/* 
 typedef struct Node {
-	int data[max];
+	int *data;
 	int index;
 }Node;
 void init(Node *t)
@@ -47,10 +47,68 @@ void getMin(Node *t,Node *q)
 		}
 	}
 	
+}*/ 
+
+typedef struct {
+    int *data;
+    int top;
+} Stack;
+
+
+Stack* Create() {
+    Stack *p=(Stack *)malloc(sizeof(Stack));
+    p->data=(int *)malloc(max*sizeof(int));
+    p->top=-1;
+    return p;
 }
 
+void Push(Stack* p, int x) {
+  if(p->top==max-1){
+      
+  }else if(p->top==-1){
+      p->top++;
+      p->data[p->top]=x;
+      p->top++;
+     p->data[p->top]=x;
+  }else{
+      int tmp=p->data[p->top];
+      p->top++;
+      p->data[p->top]=x;
+      if(tmp>x){
+        p->top++;
+        p->data[p->top]=tmp;
+      }else{
+        p->top++;
+        p->data[p->top]=x;
+      }
+  }
+}
+
+void Pop(Stack* p) {
+  if(p->top==-1){
+      
+  }else{
+      p->top--;
+      p->top--;
+  }
+}
+
+int Top(Stack* p) {
+  if(p->top==-1){
+      return;
+  }
+  return p->data[p->top-1];
+  
+}
+
+int getMin(Stack* p) {
+  return p->data[p->top];
+}
+//每一次执行push操作时，进行两次入栈，第一次入栈此元素，
+//第二次入栈此时栈中最大的元素；执行pop操作与之相对
+
 int main() {
-	Node q,t;
+/*	Node q,t;
 	init(&q);
 	init(&t);
 	push(&t);
@@ -59,4 +117,20 @@ int main() {
 	pop(&q, &top);
 	printf("%d", top);
 	return 0;
+	*/
+	int maxs;
+	int x;
+	 Stack *s;
+	  s=Create();
+	  while(scanf("%d",&x),x!=0)
+	  {
+	  	Push(s,x);
+	   } 
+	  
+	 /*top= minStackTop(&s);
+	 minStackPop(&s);*/
+	 maxs=getMin(s);
+	 printf("%d",maxs);
+	 
+	  
 } 
